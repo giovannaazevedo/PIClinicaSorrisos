3857174e0ea26691efbbb3b6e198fc09e22310c5
giovannaazevedo <giovanna.azevedo@escolar.ifrn.edu.br>
2025-01-15 11:24:09 -0300

    backend

--- frontend/src/assets/js/home.js
+++
@@ -1,7 +0,0 @@
-import '../../utils/perfil.js'
-import { fetchApi } from '../../utils/request.js'
-
-const reponse = await fetchApi(`http://localhost:3000/me`)
-
-const usernameSpan = document.getElementById('username')
-usernameSpan.textContent = reponse.Nome.split(' ')[0]
